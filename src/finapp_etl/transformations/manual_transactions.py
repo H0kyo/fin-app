60a4096f77286bb07fbb6d4008a26ef2dc3e3316
H0kyo <squ14sss@gmail.com>
2026-09-15 16:59:16 +0300

    Changed schemas logic

--- src/finapp_etl/transformations/manual_transactions.py
+++ src/finapp_etl/transformations/manual_transactions.py
@@ -2,7 +2,7 @@ from pyspark import pipelines as dp
 from pyspark.sql import functions as F
 
 
-@dp.table(name="manual_transactions", comment="Unified Silver transactions from manual bronze", schema="silver")
+@dp.table(name="silver.manual_transactions", comment="Unified Silver transactions from manual bronze")
 @dp.expect_or_drop("valid_amount", "amount IS NOT NULL")
 @dp.expect_or_drop("valid_date", "date IS NOT NULL")
 def manual_transactions():
